df06d1ec465e6c1057e83c0a57b09b656e5da7bf
chriskilinc <chriskilinc@gmail.com>
2018-11-26 22:12:40 +0100

    Dynamic Get Request

--- src/controllers/timetableController.js
+++ src/controllers/timetableController.js
@@ -1,7 +1,18 @@
 getTimetables = (req, res) => {
-  res.send({
-    foo: 'bar'
+  const stationId = req.params.id;
+
+  //  Validate Station Id ( Validate with regex and maybe even through api call to SL if it even exists )
+
+  //  Fetch SL timetables by 'stationId'
+
+  //  Build json object
+
+  //  Send json object
+  res.status(200).send({
+    stationId: stationId
   });
+
+  //  Catch Errors
 };
 
 module.exports = { getTimetables };
--- src/routes/timetable.js
+++ src/routes/timetable.js
@@ -3,9 +3,9 @@ const timetableController = require('../controllers/timetableController');
 
 router.get('/', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify('/api/timetable/:stationId'));
+  res.send(JSON.stringify('/api/timetable/:id'));
 });
 
-router.get('/:stationId', timetableController.getTimetables);
+router.get('/:id', timetableController.getTimetables);
 
 module.exports = router;
