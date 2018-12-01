ed8397bf4a904fa3ca2de86f3140bdffe8f66d96
chriskilinc <chriskilinc@gmail.com>
2018-12-01 23:58:51 +0100

    Now sends Message back incase of error

--- src/utils/realtidsinformation4.js
+++ src/utils/realtidsinformation4.js
@@ -5,7 +5,9 @@ function fetchRealtimeApi(stationId, timespan) {
   try {
     const url = `http://api.sl.se/api2/realtimedeparturesv4.json?key=${keyRealtime4}&siteid=${stationId}&timewindow=${timespan}`;
     return axios.get(url).then(res => {
-      console.log(url);
+      console.log(res.data);
+      res.data.ResponseData.Message = res.data.Message;
+
       return res.data.ResponseData;
     });
   } catch (error) {
