1c3103e7a7315d7ecedacf0bb95389953ec05ede
chriskilinc <chriskilinc@gmail.com>
2018-12-16 15:36:25 +0100

    REmoved Console Log

--- src/utils/realtidsinformation4.js
+++ src/utils/realtidsinformation4.js
@@ -6,7 +6,6 @@ function fetchRealtimeApi(stationId, timespan) {
   try {
     const url = `http://api.sl.se/api2/realtimedeparturesv4.json?key=${keyRealtime4}&siteid=${stationId}&timewindow=${timespan}`;
     return axios.get(url).then(res => {
-      // console.log(res.data);
       console.log(`Fetching api id: ${stationId}, timespan: ${timespan} `);
       res.data.ResponseData.Message = res.data.Message;
       return res.data.ResponseData;
